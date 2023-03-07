9d4c1b050526e8224618094672f24d89ee4ec8bf
PavelSteel <misskiisteel@gmail.com>
2023-03-07 23:04:19 +0300

    HW_Lesson01_Pell

---
+++ src/HW_Lesson01/Pell.java
@@ -0,0 +1,22 @@
+package HW_Lesson01;
+
+import java.util.Scanner;
+
+public class Pell {
+    public static void main(String[] args) throws Exception {
+
+        //а (n) = 2 * а (n-1) + а (n-2)
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            long a0 = 0;
+            long a1 = 1;
+            long res = 0;
+            for (int i = 3; i <= n; i++) {
+                res = 2 * a1 + a0;
+                a0 = a1;
+                a1 = res;
+            }
+            System.out.println(res);
+        }
+    }
+}
