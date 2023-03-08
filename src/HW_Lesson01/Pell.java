a79119d1af320e3b366040333d2c46833163f1dc
PavelSteel <misskiisteel@gmail.com>
2023-03-08 20:50:54 +0300

    HW_Lesson01_refactor Pell

--- src/HW_Lesson01/Pell.java
+++ src/HW_Lesson01/Pell.java
@@ -8,15 +8,19 @@ public class Pell {
         //а (n) = 2 * а (n-1) + а (n-2)
         try (Scanner scanner = new Scanner(System.in)) {
             int n = scanner.nextInt();
-            long a0 = 0;
-            long a1 = 1;
-            long res = 0;
-            for (int i = 3; i <= n; i++) {
-                res = 2 * a1 + a0;
-                a0 = a1;
-                a1 = res;
+            if(n>=0 && n<=30) {
+                long a0 = 0;
+                long a1 = 1;
+                long res = 0;
+                for (int i = 3; i <= n; i++) {
+                    res = 2 * a1 + a0;
+                    a0 = a1;
+                    a1 = res;
+                }
+                System.out.println(res);
+            } else {
+                throw new RuntimeException("Введены некорректные данные");
             }
-            System.out.println(res);
         }
     }
 }
